56ed04d3c6a0322f17f75d2ad1fcbe204b13303a
rajat-galav <rajatgalav1295@gmail.com>
2019-09-09 10:56:52 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ class App extends React.Component {
       matrix: ['', '', '', '', '', '', '', '', ''],
       winningPattern: ['012', '345', '678', '036', '147', '257', '047', '246'],
       patternX: [],
-      patternO: []
+      patternO: [],
+      winner: ''
     };
   }
   divClicked = (e) => {
-    let { matrix, patternX, patternO } = this.state;
-    console.log('div clicked', e.target.id);
+    var { matrix, patternX, patternO } = this.state;
     const matrixId = parseInt(e.target.id.substr(-1));
+    console.log(e.target.id);
+    
     if (this.state.chance === 1) {
       patternX.push(matrixId);
       matrix[matrixId] = this.player1;
@@ -26,8 +28,17 @@ class App extends React.Component {
       patternO.push(matrixId);
       matrix[matrixId] = this.player2;
     }
-    this.checkWinner(matrixId);
-    this.setState({ matrix, patternX, patternO, chance: this.state.chance === 1 ? 0 : 1});
+    this.setState({ matrix });
+    const winner = this.checkWinner(matrixId);
+    if (winner) {
+      if (this.state.chance === 1) {
+        this.setState({ winner: 'player1 wins' })
+      } else {
+        this.setState({ winner: 'player2 wins' });
+      }
+    } else {
+      this.setState({ patternX, patternO, chance: this.state.chance === 1 ? 0 : 1}, console.log('patternX', patternX));
+    }
   }
   checkWinner = (matrixId) => {
     const { chance } = this.state;
@@ -35,39 +46,55 @@ class App extends React.Component {
     const player2Array = this.state.patternO.sort();
     var firstMoves = [];
     var secondMoves = [];
-    if (player1Array.length > 2 && chance === 1) {
-      for (var i = 0 ; i < player1Array.length-2; i++) {
-        for ( var j = i+1; j < player1Array.length-1; i++) {
-          const flag = _.includes(this.state.winningPattern, ''+player1Array[i]+player1Array[j]+matrixId);
-          console.log('flag is', flag);
+    if (player1Array.length >= 2 && chance === 1) {
+      for (var i = 0 ; i < player1Array.length-1; i++) {
+        for ( var j = i+1; j < player1Array.length; j++) {
+          const stringformat = this.convertTostring(player1Array[i], player1Array[j], matrixId);
+          const flag = _.includes(this.state.winningPattern, stringformat);
+          if (flag) {
+            return flag;
+          }
         }
       }
     }
-    if (player2Array.length > 2 && chance === 0){
-      for (var i = 0; i < player2Array.length-2; i++) {
-        secondMoves.push([player2Array[i], player2Array[i+1], player2Array[i+2]]);
+    if (player2Array.length >= 2 && chance === 0) {
+      for (var i = 0 ; i < player2Array.length-1; i++) {
+        for ( var j = i+1; j < player2Array.length; j++) {
+          const stringformat = this.convertTostring(player2Array[i], player2Array[j], matrixId);
+          const flag = _.includes(this.state.winningPattern, stringformat);
+          if (flag) {
+            return flag;
+          }
+        }
       }
     }
-    return true;
+    return false;
+  }
+  convertTostring = (a, b, c) => {
+    let arrayFormat = [a, b, c].sort();
+    return ''+arrayFormat[0]+arrayFormat[1]+arrayFormat[2];
   }
   render() {
     const { matrix } = this.state;
     return (
-      <div onClick={this.divClicked}>
-        <div style={{ display: 'flex' }}>
-          <div className="game-box" id="first-row-0" >{matrix[0]}</div>
-          <div className="game-box" id="first-row-1" >{matrix[1]}</div>
-          <div className="game-box" id="first-row-2" >{matrix[2]}</div>
-        </div>
-        <div style={{ display: 'flex' }}>
-          <div className="game-box" id="second-row-3" >{matrix[3]}</div>
-          <div className="game-box" id="second-row-4" >{matrix[4]}</div>
-          <div className="game-box" id="second-row-5" >{matrix[5]}</div>
-        </div>
-        <div style={{ display: 'flex' }}>
-          <div className="game-box" id="third-row-6" >{matrix[6]}</div>
-          <div className="game-box" id="third-row-7" >{matrix[7]}</div>
-          <div className="game-box" id="third-row-8" >{matrix[8]}</div>
+      <div>
+        <div>{this.state.winner}</div>
+        <div >
+          <div style={{ display: 'flex' }}>
+            <div className="game-box" id="first-row-0" onClick={this.divClicked} >{matrix[0]}</div>
+            <div className="game-box" id="first-row-1" onClick={this.divClicked} >{matrix[1]}</div>
+            <div className="game-box" id="first-row-2" onClick={this.divClicked} >{matrix[2]}</div>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <div className="game-box" id="second-row-3" onClick={this.divClicked} >{matrix[3]}</div>
+            <div className="game-box" id="second-row-4" onClick={this.divClicked} >{matrix[4]}</div>
+            <div className="game-box" id="second-row-5" onClick={this.divClicked} >{matrix[5]}</div>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <div className="game-box" id="third-row-6" onClick={this.divClicked} >{matrix[6]}</div>
+            <div className="game-box" id="third-row-7" onClick={this.divClicked} >{matrix[7]}</div>
+            <div className="game-box" id="third-row-8" onClick={this.divClicked} >{matrix[8]}</div>
+          </div>
         </div>
       </div>
     )
